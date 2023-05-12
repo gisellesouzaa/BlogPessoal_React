a88f61e284e43eb7f7b8c0e5454c8ab60272809a
Giselle Souza <reallovetour@gmail.com>
2023-05-12 08:43:01 -0300

    Criação do projeto

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+    return (
+        <>
+            <h1 className='titulo'>Hello World React</h1>
+            <img className='imagem' src="https://www.infoescola.com/wp-content/uploads/2008/05/cavalo.jpg" alt=""></img>
+        </>
+    )
+}
+
+export default Home;
